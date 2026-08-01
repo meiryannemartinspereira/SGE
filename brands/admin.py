fe0cf8a408497e99bf22788a64d0cea14c8b436b
Meiryanne Martins <meirympereira@gmail.com>
2026-08-01 16:10:22 -0300

    feat: add brand adm

--- brands/admin.py
+++ brands/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description',)
+    search_fields = ('name',)
+
+admin.site.register(models.Brand, BrandAdmin)
