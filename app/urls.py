5925b195ce38d08d232d95a2db1006a30008a1b8
Meiryanne Martins <meirympereira@gmail.com>
2026-08-03 13:13:40 -0300

    fix: base html

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.shortcuts import render
+
+def home_view(request):
+    return render(
+        request,
+        'base.html',
+    )
+
 urlpatterns = [
+    path('home/', home_view), 
     path('admin/', admin.site.urls),
     path('', include('brands.urls')),
     path('', include('suppliers.urls'))
