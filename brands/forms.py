5590217a2495052476e65d3a14c8a3cb4674b223
Meiryanne Martins <meirympereira@gmail.com>
2026-08-04 11:56:01 -0300

    style: improve brand list page layout

--- brands/forms.py
+++ brands/forms.py
@@ -7,3 +7,12 @@ class BrandForm(forms.ModelForm):
     class Meta:
         model = models.Brand
         fields = ['name', 'description']
+        widgets = {
+            'name': forms.TextInput(attrs={'class':'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows':3}),
+        }  
+
+        labels = {
+            'name': 'Nome',
+            'description': 'Descrição',
+        } 
