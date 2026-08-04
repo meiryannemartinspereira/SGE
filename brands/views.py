777b53229e452af6946912c692de03373d0e9f4d
Meiryanne Martins <meirympereira@gmail.com>
2026-08-04 11:21:03 -0300

    feat: add create brand

--- brands/views.py
+++ brands/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import ListView
-from . import models
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
+from . import models, forms
 
 class BrandListView(ListView):
     model = models.Brand
@@ -14,3 +15,9 @@ class BrandListView(ListView):
             queryset = queryset.filter(name__icontains=name)
 
         return queryset
+
+class BrandCreateView(CreateView):
+    model = models.Brand
+    template_name = 'brand_create.html'
+    form_class = forms.BrandForm
+    success_url = reverse_lazy('brand_list')
