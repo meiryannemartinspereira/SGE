60e238c76f13fd8ccc2ab856d6d8523c6b3b8ce7
Meiryanne Martins <meirympereira@gmail.com>
2026-08-04 13:42:35 -0300

    feat: add update brand

--- brands/views.py
+++ brands/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 from django.urls import reverse_lazy
 from . import models, forms
 
@@ -25,3 +25,9 @@ class BrandCreateView(CreateView):
 class BrandDetailView(DetailView):
     model = models.Brand
     template_name = 'brand_detail.html'
+
+class BrandUpdateView(UpdateView):
+    model = models.Brand
+    template_name = 'brand_update.html'
+    form_class = forms.BrandForm
+    success_url = reverse_lazy('brand_list')
