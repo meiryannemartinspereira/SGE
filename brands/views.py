37ab88e2e2aa1737386ff846e532768b1da08bc8
Meiryanne Martins <meirympereira@gmail.com>
2026-08-04 12:15:47 -0300

    feat: create brand detail

--- brands/views.py
+++ brands/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView
 from django.urls import reverse_lazy
 from . import models, forms
 
@@ -21,3 +21,7 @@ class BrandCreateView(CreateView):
     template_name = 'brand_create.html'
     form_class = forms.BrandForm
     success_url = reverse_lazy('brand_list')
+
+class BrandDetailView(DetailView):
+    model = models.Brand
+    template_name = 'brand_detail.html'
