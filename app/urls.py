e3a44115bcd071b33680ba1bfd8201a9e2a5a167
Meiryanne Martins <meirympereira@gmail.com>
2026-08-06 20:55:25 -0300

    feat: add app categories

--- app/urls.py
+++ app/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('home/', home_view), 
     path('admin/', admin.site.urls),
     path('', include('brands.urls')),
+    path('', include('categories.urls')),
     path('', include('suppliers.urls'))
 ]
