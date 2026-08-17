d64b02b36c18b19984d749a324f120b418fd8b96
Meiryanne Martins <meirympereira@gmail.com>
2026-08-17 14:50:44 -0300

    fix: update category and create metrics for products

--- app/views.py
+++ app/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
+from . import metrics
 
 def home(request):
-    product_metrics = {
-        'total_quantity': 1000,
-        'total_cost_price': 100000,
-        'total_selling_price': 300000,
-        'total_profit': 200000,
-    }
+    product_metrics = metrics.get_product_metrics()
     context = {
         'product_metrics': product_metrics
     }
