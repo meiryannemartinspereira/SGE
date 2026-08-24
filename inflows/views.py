4aeb56c286beff01446c5238f16647342de2397f
Meiryanne Martins <meirympereira@gmail.com>
2026-08-24 13:18:01 -0300

    feat: add api on app inflow

--- inflows/views.py
+++ inflows/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import ListView, CreateView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from rest_framework import generics
 from django.urls import reverse_lazy
-from . import models, forms
+from . import models, forms, serializers
 
 class InflowListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     model = models.Inflow
@@ -30,4 +31,13 @@ class InflowDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = models.Inflow
     template_name = 'inflow_detail.html'
     permission_required = 'inflows.view_inflow'
+
+class InflowCreateListAPIView(generics.ListCreateAPIView):
+    queryset = models.Inflow.objects.all()
+    serializer_class = serializers.InflowSerializer
+
+
+class InflowRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = models.Inflow.objects.all()
+    serializer_class = serializers.InflowSerializer
 
