ce50397910a500eb62f8ef253c213b1c79ba92ca
Meiryanne Martins <meirympereira@gmail.com>
2026-08-24 13:23:33 -0300

    feat: add api on app outflow

--- outflows/views.py
+++ outflows/views.py
@@ -1,8 +1,9 @@
 from django.views.generic import ListView, CreateView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from rest_framework import generics
 from app import metrics
 from django.urls import reverse_lazy
-from . import models, forms
+from . import models, forms, serializers
 
 class OutflowListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     model = models.Outflow
@@ -35,5 +36,13 @@ class OutflowDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView)
     model = models.Outflow
     template_name = 'outflow_detail.html'
     permission_required = 'outflows.view_outflow'
+    
+class OutflowCreateListAPIView(generics.ListCreateAPIView):
+    queryset = models.Outflow.objects.all()
+    serializer_class = serializers.OutflowSerializer
 
+
+class OutflowRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = models.Outflow.objects.all()
+    serializer_class = serializers.OutflowSerializer
 
