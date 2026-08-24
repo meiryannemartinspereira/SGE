6bb0fbe5441964dcba09afd7ac750dc4e37a5be9
Meiryanne Martins <meirympereira@gmail.com>
2026-08-24 13:35:11 -0300

    feat: add api on app products

--- products/urls.py
+++ products/urls.py
@@ -29,5 +29,8 @@ urlpatterns = [
         views.ProductDeleteView.as_view(),
         name='product_delete',
     ),
+
+    path('api/v1/products/', views.ProductCreateListAPIView.as_view(), name='product-create-list-api-view'),
+    path('api/v1/products/<int:pk>/', views.ProductRetrieveUpdateDestroyAPIView.as_view(), name='product-detail-api-view'),
 ]
 
